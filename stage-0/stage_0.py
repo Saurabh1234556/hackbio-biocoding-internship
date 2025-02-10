c59edc3e961724f9ef86012b26af9847548f5e3e
Saurabh1234556 <163909138+Saurabh1234556@users.noreply.github.com>
2025-02-10 22:07:05 +0530

    Update stage_0.py

--- stage-0/stage_0.py
+++ stage-0/stage_0.py
@@ -1,4 +1,5 @@
 # Hackbio Biocoding Internship - Stage 0 Task
+#https://github.com/Saurabh1234556/hackbio-biocoding-internship
 '''
 [1] Hey! I am saurabh mazumdar 
 [2] The codon assigned to me was UCU (Serine)
